e0aa26af81e397b227618e85b6d67fa9447001e1
Firuza <firuza.m.mammadova@gmail.com>
2020-10-31 21:44:36 +0400

    done

---
+++ FreelanceProject/Services/Services/JobService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Models;
+using Repository;
+
+namespace Services.Services
+{
+    public interface IJobService
+    {
+        Task<Job> GetById(string id);
+        Task<IEnumerable<Job>> GetAll();
+        Task<Guid> Add(Job entity);
+        Task Delete(string id);
+        Task Update(Job entity);
+
+    }
+
+    public class JobService : IJobService
+    {
+        private readonly IJobRepository _jobRepository;
+
+        public JobService(IJobRepository jobRepository)
+        {
+            _jobRepository = jobRepository;
+        }
+
+        public async Task<Guid> Add(Job entity)
+        {
+            var result = await _jobRepository.Add(entity);
+            return result;
+        }
+
+        public async Task Delete(string id)
+        {
+            await _jobRepository.Delete(id);
+        }
+
+        public async Task<IEnumerable<Job>> GetAll()
+        {
+            var result = await _jobRepository.GetAll();
+            return result;
+        }
+
+        public async Task<Job> GetById(string id)
+        {
+            var result = await _jobRepository.GetById(id);
+            return result;
+        }
+
+        public async Task Update(Job entity)
+        {
+            await _jobRepository.Update(entity);
+        }
+    }
+}
