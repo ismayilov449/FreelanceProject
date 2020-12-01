5bf23dec79f349c4938cc836f03984fcc5572476
Rufat Ismayilov <rufet.ismayilov.1999@gmail.com>
2020-12-01 14:02:38 +0400

    not finished yet

--- FreelanceProject/Api/Infrastructure/SignalR/NotifyService.cs
+++ FreelanceProject/Api/Infrastructure/SignalR/NotifyService.cs
@@ -10,15 +10,28 @@ namespace Api.Infrastructure.SignalR
     public interface INotifyService
     {
         Task SendNotification(Job job);
+        Task RefreshNotification(Job job, string userId);
     }
 
     public class NotifyService : INotifyService
     {
         private readonly IHubContext<NotificationHub> _hubContext;
+        private readonly IClientManager _clientManager;
 
-        public NotifyService(IHubContext<NotificationHub> hubContext)
+        public NotifyService(IHubContext<NotificationHub> hubContext, IClientManager clientManager)
         {
             _hubContext = hubContext;
+            _clientManager = clientManager;
+        }
+
+        public async Task RefreshNotification(Job job, string userId)
+        {
+            var ids = _clientManager.GetClients().FindAll(x => x.UserId != userId).Select(x => x.ConnectionId).ToList();
+
+            if (ids != null)
+            {
+                await _hubContext.Clients.Clients(ids).SendAsync("ReceiveNotification", job);
+            }
         }
 
         public async Task SendNotification(Job job)
